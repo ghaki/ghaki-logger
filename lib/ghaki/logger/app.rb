fb979499084efd2ca033fab9849b8f188e1cd1c7
Gerald Kalafut <gkalafut@ebay.com>
2011-03-28 04:05:33 -0700

    now using engine plugin

--- lib/ghaki/logger/app.rb
+++ lib/ghaki/logger/app.rb
@@ -1,42 +1,12 @@
 ############################################################################
-require 'singleton'
+require 'ghaki/app/plugin'
 require 'ghaki/logger/base'
 
-############################################################################
 module Ghaki module Logger
-  class App
-    include Singleton
 
-    #---------------------------------------------------------------------
-    DEF_LOGGER_OPTS = {}
-
-    #---------------------------------------------------------------------
-    attr_writer :logger, :logger_opts
-
-    #---------------------------------------------------------------------
-    def logger
-      @logger ||= Ghaki::Logger::Base.new( self.logger_opts )
-    end
-
-    #---------------------------------------------------------------------
-    def logger_opts
-      @logger_opts ||= DEF_LOGGER_OPTS.dup
-    end
-
+  class App < Ghaki::App::Plugin
+    create_engine_plugin :logger, Base
+    link_engine_to_plugin
   end # class 
+
 end end # package
-############################################################################
-
-############################################################################
-begin
-  require 'ghaki/app/engine'
-  Ghaki::App::Engine.class_eval do
-    def logger      ; Ghaki::Logger::App.instance.logger       end
-    def logger= val ; Ghaki::Logger::App.instance.logger = val end
-
-    def logger_opts      ; Ghaki::Logger::App.instance.logger_opts       end
-    def logger_opts= val ; Ghaki::Logger::App.instance.logger_opts = val end
-  end
-rescue LoadError
-end
-############################################################################
--- spec/ghaki/logger/app_engine_spec.rb
+++ spec/ghaki/logger/app_engine_spec.rb
@@ -1,37 +1,42 @@
 ############################################################################
+require 'ghaki/app/engine'
 require 'ghaki/logger/app'
 
-############################################################################
-module Ghaki module Logger module AppEngineTesting
-  begin
-    require 'ghaki/app/engine'
+describe Ghaki::App::Engine do
 
-    describe Ghaki::App::Engine do
-      context 'singleton' do
-        subject { Ghaki::App::Engine.instance }
-        it { should respond_to :logger  }
-        it { should respond_to :logger= }
-        it { should respond_to :logger_opts  }
-        it { should respond_to :logger_opts= }
-      end
-      context 'singleton methods' do
-        describe '#logger' do
-          subject { Ghaki::App::Engine.instance.logger }
-          specify { should be_an_instance_of(Ghaki::Logger::Base) }
-          specify { should equal(Ghaki::Logger::App.instance.logger) }
-        end
-        describe '#logger_opts' do
-          subject { Ghaki::App::Engine.instance.logger_opts }
-          specify { should be_an_instance_of(::Hash) }
-        end
-      end
-    end
-
-  rescue LoadError
-    describe 'Ghaki::App::Engine' do
-      pending 'external library not available: <ghaki/app/engine>'
-    end
+  before(:all) do
+    @eng_app = Ghaki::App::Engine.instance
+    @log_app = Ghaki::Logger::App.instance
   end
 
-end end end
-############################################################################
+  context 'singleton' do
+    subject { @eng_app }
+    it { should respond_to :logger  }
+    it { should respond_to :logger= }
+    it { should respond_to :logger_opts  }
+    it { should respond_to :logger_opts= }
+    it { should respond_to :logger_defs  }
+    it { should respond_to :logger_defs= }
+  end
+
+  context 'singleton methods' do
+
+    describe '#logger' do
+      subject { @eng_app.logger }
+      specify { should be_an_instance_of(Ghaki::Logger::Base) }
+      specify { should equal(@log_app.logger) }
+    end
+
+    describe '#logger_opts' do
+      subject { @eng_app.logger_opts }
+      specify { should be_an_instance_of(::Hash) }
+    end
+
+    describe '#logger_defs' do
+      subject { @eng_app.logger_defs }
+      specify { should be_an_instance_of(::Hash) }
+    end
+
+  end
+
+end
